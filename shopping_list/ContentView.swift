f2eb6b7c52c4a98bae8cdfe8967b0c11cfa8eea4
Stephanie <61058084+hurabono@users.noreply.github.com>
2025-04-05 20:01:53 -0400

    Update ContentView.swift

--- shopping_list/ContentView.swift
+++ shopping_list/ContentView.swift
@@ -5,16 +5,21 @@
 //  Created by Heesu Cho on 2025-02-27.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Welcome to Shopping")
-            .font(.title)
-            .padding()
+        NavigationView {
+            LoginView()
+        }
+        .navigationBarHidden(true)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
